cecad8ac534ce5f4aa967b91ed1176cd8737bda1
Binamra7 <binamra7khadka6@gmail.com>
2021-07-05 20:10:05 +0545

    fixed qns2

--- Lab10/l10_qns2.cpp
+++ Lab10/l10_qns2.cpp
@@ -30,12 +30,7 @@ int main()
         
 
     }
-
-    // int checkBalance;
-    // printf("\n********************************************************************************************\n");
-    // printf("Enter the Balance:");
-    // scanf("%d", &checkBalance);
-    printf("Account No.\tName\t\t\tBalance\n");
+   printf("Account No.\tName\t\t\tBalance\n");
 
     for (int i = 0; i < n;i++)
     {
@@ -44,17 +39,55 @@ int main()
             printf("%d\t\t%s\t\t\t%d\n",info[i].accNum, info[i].name, info[i].balance);
         }
     }
-    int choose;
-    printf("1.WithDrawl\n2.Deposit");
+
+    int index,choose, acNum;
+
+    printf("Enter the account number:");
+    scanf("%d", &acNum);
+    for (int i = 0;i<n;i++)
+    {
+        if(info[i].accNum==acNum){
+           index = i;
+           break;
+        }   
+    }
+        printf("1.WithDrawl\n2.Deposit");
     scanf("%d", &choose);
+    
     switch (choose)
     {
     case 1:
-        
+        start:
+        printf("Enter the amount you want to widthdraw\n");
+        int with;
+        scanf("%d", &with);
+        if(with>info[index].balance){
+            printf("The amount is greater than the balance\nTRY AGAIN");
+            goto start;
+            break;
+        }
+        else{
+            info[index].balance-=with;
+            printf("Withdrawl successuful\n");
+        }
         break;
-    
+    case 2:
+        printf("Enter the amount you want to deposit:");
+        int depo;
+        scanf("%d", &depo);
+        info[index].balance += depo;
+        break;
     default:
         break;
+    }
+    printf("\n******************************************************************************\n");
+    printf("Account No.\tName\t\t\tBalance\n");
+
+    for (int i = 0; i < n;i++)
+    {
+        
+            printf("%d\t\t%s\t\t\t%d\n",info[i].accNum, info[i].name, info[i].balance);
+    
     }
 
     return 0;
