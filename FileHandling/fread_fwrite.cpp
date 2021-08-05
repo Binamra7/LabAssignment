f91e4034c79bc50fc175291650e5afe01cfd6d96
Binamra7 <binamra7khadka6@gmail.com>
2021-08-05 11:25:04 +0545

    changes

---
+++ FileHandling/fread_fwrite.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct student {
+    int age,clas;
+    char name[30];
+};
+
+int main()
+{
+    struct student rec;
+    FILE *fp;
+    int n, i;
+    printf("How many students?");
+    scanf("%d", &n);
+    fp = fopen("student.txt", "a+");
+    for (int i = 1; i <= n;i++)
+    {
+        printf("Enter %d Name,age and class:",i);
+        fflush(stdin);
+        gets(rec.name);
+        fflush(stdin);
+        scanf("%d%d", &rec.age, &rec.clas);
+        fwrite(&rec, sizeof(rec), 1, fp);
+    }
+    rewind(fp);
+    printf("\nName\tAge\tClass\n");
+    printf("________________________________________________\n");
+    while(fread(&rec,sizeof(rec),1,fp)==1)
+        printf("%s\t%d\t%d\n", rec.name, rec.age, rec.clas);
+    fclose(fp);
+
+    return 0;
+}
---
+++ FileHandling/name_starting_from_s.cpp
@@ -0,0 +1,17 @@
+//WAP to store the information of 'n' students in a file sample.txt and display only those information whose name starts with 'S'.
+
+#include<stdio.h>
+#include<conio.h>
+
+struct students {
+    int roll;
+    char name[20];
+}
+
+
+int main()
+{
+    int n, num;
+    
+    return 0;
+}
