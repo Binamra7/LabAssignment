dff8a95912c9203c79247ef5a0162dc342b3f815
Binamra7 <binamra7khadka6@gmail.com>
2021-08-08 13:14:49 +0545

    changes

--- FileHandling/deletingInfo.cpp
+++ FileHandling/deletingInfo.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
+#include<string.h>
 
 #define ll long long;
 
@@ -11,7 +12,8 @@ struct student{
 int main()
 {
     struct student info;
-    FILE *fp;
+    FILE *fp,*fp1;
+    char queryName[20];
     int n;
     printf("How many students?");
     scanf("%d", &n);
@@ -32,7 +34,34 @@ int main()
     printf("\nName\tAddress\tPhone\n");
     printf("___________________________\n");
     while (fread(&info, sizeof(info), 1, fp) == 1)
-        printf("%s\t%d\t%d\n", info.name, info.add, info.ph);
+        printf("%s\t%s\t%d\n", info.name, info.add, info.ph);
     fclose(fp);
+
+    fflush(stdin);
+
+    printf("Enter the name you want to delete:");
+    gets(queryName);
+    fp = fopen("student999.txt", "r");
+    fp1 = fopen("temp.txt", "w");
+
+    while(fread(&info,sizeof(info),1,fp)==1)
+    {
+        if(strcmp(queryName,info.name)!=0){
+            fwrite(&info, sizeof(info), 1, fp1);
+        }
+    }
+    fclose(fp);
+    fclose(fp1);
+    remove("student999.txt");
+    rename("temp.txt", "student999.txt");
+    printf("\nThe record after removing:\n");
+    printf("\nName\tAddress\tPhone\n");
+    printf("_________________________________\n");
+    fp1 = fopen("student999.txt", "r");
+    while(fread(&info,sizeof(info),1,fp1)==1)
+    {
+        printf("%s\t%s\t%d\n", info.name, info.add, info.ph);
+    }
+    fclose(fp1);
     return 0;
 }
